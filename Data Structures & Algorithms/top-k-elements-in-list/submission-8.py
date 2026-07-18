e3a6df95e38adc4f3c703bc177f06c0d9676d36d
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-07-18 18:23:11 +0100

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+            freqDict = {}
+            freqs = []
+            outList = []
+
+            for i in nums:
+                key = i
+
+                if key not in freqDict:
+                    freqDict[key] = 1
+                else:
+                    freqDict[key] += 1
+
+            while len(outList) < k:
+                highFreq = 0
+                lock = 0
+
+                for key, v in freqDict.items():
+                    if v > highFreq:
+                        highFreq = v
+                        lock = key
+
+                outList.append(lock)
+                del freqDict[lock]
+                
+
+            return(outList)
